d144dac499260b548fabce9c32da2df03f704542
Bartek Pacia <barpac02@gmail.com>
2024-01-11 15:23:46 +0100

    remove foojay-resolver, doesnt work anyway

--- mobile/settings.gradle.kts
+++ mobile/settings.gradle.kts
@@ -14,9 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-}
-
 rootProject.name = "Notes"
 include(":app")
