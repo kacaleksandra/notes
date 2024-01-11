cfad919b0f79e732e34e023890df17360aaf6459
Aleksandra <elinda100@gmail.com>
2024-01-11 19:15:17 +0100

    added swagger, prisma schema and some dependencies

---
+++ backend/prisma/migrations/20240110182700_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes app')
+    .setDescription('The notes API description')
+    .setVersion('1.0')
+    .addTag('notes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
