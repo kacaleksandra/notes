ab3aa8f1e8912185038b4c19b8c7b339d97ff539
Bartek Pacia <barpac02@gmail.com>
2025-01-26 03:27:47 +0100

    configure Gradle buildscripts to include `flutter_module`

--- mobile/settings.gradle.kts
+++ mobile/settings.gradle.kts
@@ -7,7 +7,8 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // TODO: There are some problems with repositories not being resolved. Report to Fluttrr.
+    repositoriesMode = RepositoriesMode.PREFER_PROJECT // was FAIL_ON_PROJECT_REPOS, but it doesn't work with Flutter
     repositories {
         google()
         mavenCentral()
@@ -16,3 +17,8 @@ dependencyResolutionManagement {
 
 rootProject.name = "notes"
 include(":app")
+
+// Replace "flutter_module" with whatever package_name you supplied when you ran:
+// `$ flutter create -t module [package_name]
+val filePath = settingsDir.parentFile.toString() + "/flutter_module/.android/include_flutter.groovy"
+apply(from = File(filePath))
